abba95508749302e2cf7b2d46130dccfcf435d00
Lucas Varjão <lucasvarjao.dev@gmail.com>
2026-01-25 00:17:19 -0300

    Update dependencies: set Prettier to version 3.8.1 and add Prettier plugin for Tailwind CSS. Refactor login template for improved readability and consistency in input formatting.

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,9 @@
-/** @type {import('prettier').Config} */
+// prettier.config.js
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+}
 module.exports = {
   trailingComma: 'es5',
   tabWidth: 2,
